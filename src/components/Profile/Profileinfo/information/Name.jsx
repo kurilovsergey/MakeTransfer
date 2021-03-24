d30403d4376f7c18e293122b9aafb5dae5be6bb2
Kurilov Sergey <47578235+kurilovsergey@users.noreply.github.com>
2021-03-24 23:25:50 +0300

    add Hook in status Component

--- src/components/Profile/Profileinfo/information/Name.jsx
+++ src/components/Profile/Profileinfo/information/Name.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import s from './Name.module.css';
 import ProfileStatus from './ProfileStatus.jsx'
+import ProfileStatusWithHook from './ProfileStatusWithHook'
 
 const Name =(props) => {
   console.log('prrops ',props);
 return(
   <div >
      <div >{props.autorizatedUserID}</div>
-     <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+     <ProfileStatusWithHook status={props.status} updateStatus={props.updateStatus}/>
   </div>   
 )
 }
---
+++ src/components/Profile/Profileinfo/information/ProfileStatusWithHook.jsx
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+
+
+
+const ProfileStatusWithHook = (props) => {
+
+  let [editMode, setEditMode] = useState(false)
+  let [status, setStatus] = useState(props.status)
+  
+  const activateEditMode = () => {
+    setEditMode(true);
+  }
+
+  const deactivateEditMode = () =>{
+    setEditMode(false);
+    props.updateStatus(status)
+  }
+
+  const onStatusChange = (e) => {
+    setStatus(e.currentTarget.value)
+  }
+
+return( 
+    <div>
+      { !editMode &&
+    <div> 
+      <span onDoubleClick={activateEditMode}>{props.status || "no status"}</span>
+    </div>
+  } 
+  { editMode &&
+    <div>
+        <input onChange={onStatusChange} value={status} autoFocus={true} onBlur={deactivateEditMode}></input>
+    </div>
+  }
+  </div>
+)
+}
+
+
+export default ProfileStatusWithHook;
