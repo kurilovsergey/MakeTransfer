4cd5606df44629316d98b457fc8dc24024cb91d6
Kurilov Sergey <47578235+kurilovsergey@users.noreply.github.com>
2021-03-25 21:28:55 +0300

    add useEffect in Status

--- src/components/Profile/Profileinfo/information/ProfileStatusWithHook.jsx
+++ src/components/Profile/Profileinfo/information/ProfileStatusWithHook.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 
 
@@ -6,6 +6,11 @@ const ProfileStatusWithHook = (props) => {
 
   let [editMode, setEditMode] = useState(false)
   let [status, setStatus] = useState(props.status)
+
+  useEffect(() => {
+    setStatus(props.status)},
+    [props.status]
+  );
   
   const activateEditMode = () => {
     setEditMode(true);
