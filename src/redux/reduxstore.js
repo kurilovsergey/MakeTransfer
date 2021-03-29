bbb00ba21ab8c80ddd36de367c8c796a385870f1
Kurilov Sergey <47578235+kurilovsergey@users.noreply.github.com>
2021-03-29 21:23:09 +0300

    add redux dev tools

--- src/redux/reduxstore.js
+++ src/redux/reduxstore.js
@@ -1,11 +1,10 @@
-import {createStore, combineReducers, applyMiddleware} from "redux";
+import {createStore, combineReducers, applyMiddleware, compose} from "redux";
 import thunkMiddleware from 'redux-thunk';
 import profile_reducer from './profile-reducer';
 import dialog_reducer from './dialogs-reducer';
 import users_reducer from './users-reducer';
 import Auth_reducer from './Auth-reducer';  
 import App_reducer from './app-reducer';
-
 
 
 let reducers = combineReducers({
@@ -16,7 +15,13 @@ let reducers = combineReducers({
     App: App_reducer,
 });
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, /* preloadedState, */ composeEnhancers(
+    applyMiddleware(thunkMiddleware)
+  ));
+
+
+//let store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
 export default store;
 
